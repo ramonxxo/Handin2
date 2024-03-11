0cbfd223c79f34c7e8c5a71147c2be2421d6402e
ramonxxo <99993685+ramonxxo@users.noreply.github.com>
2024-03-11 15:53:04 +0100

    init
    SE_19 LU Assessment 2/4

---
+++ index.js
@@ -0,0 +1,91 @@
+const App = require('./src/app')
+const BodyParser = require('./src/body-parser')
+const Router = require('./src/router')
+
+const app = App()
+const router = Router()
+
+
+app.get('/test/test2', function test() { }, function test2() { })
+app.post('/test', (req, res) => console.log('test'))
+app.patch('/test', (req, res) => console.log('test'))
+app.put('/test', (req, res) => console.log('test'))
+app.del('/test', (req, res) => console.log('test'))
+
+
+const mid1 = (req, res, next) => {
+    console.log('mid1')
+    next()
+}
+
+const mid2 = (req, res, next) => {
+    console.log('mid2')
+    next()
+}
+
+const controller = (req, res) => {
+    console.log('controller')
+    res.end('controller')
+}
+
+app.useAll(BodyParser)
+
+app.get('/middleware', mid1, mid2, controller)
+
+
+app.get('/params/:id/:name', (req, res) => {
+    res.end(JSON.stringify({ params: req.params, query: req.query }, null, 2))
+})
+
+app.get('/response/:id', (req, res) => {
+    if (req.params.id === '123') {
+        res.status(200).json(req.params)
+        return
+    }
+
+    res.status(400).json({ message: 'Invalid id' })
+})
+
+router.get('/users', (req, res) => res.end('User route from router instance'))
+router.get('/admins', (req, res) => res.end('Admins route'))
+
+router.useAll((req, res, next) => {
+    console.log('middleware for router instance /admins and /users')
+    next()
+})
+
+router.use('/users', (req, res, next) => {
+    console.log('middleware for /users')
+    next()
+})
+
+router.use('/admins', (req, res, next) => {
+    console.log('middleware for /admins')
+    next()
+})
+
+
+app.useRouter('', router)
+
+app.use('/admins', (req, res, next) => {
+    console.log('middleware for all admins routes')
+    next()
+})
+
+app.useAll((req, res, next) => {
+    console.log('middleware for all routes')
+    next()
+})
+
+
+app.static('./files')
+
+app.post('/body-parser', (req, res) => {
+    res.send(`name: ${req.body.name}, age: ${req.body.age}`)
+})
+
+const start = async () => {
+    app.run(3000)
+}
+
+start()
---
+++ src/app.js
@@ -0,0 +1,185 @@
+const { createServer } = require('http')
+const { match } = require('path-to-regexp')
+const requestDecorator = require('./request')
+const responseDecorator = require('./response')
+const { readdir } = require('fs/promises')
+const { statSync, createReadStream } = require('fs')
+const path = require('path')
+const { pipeline } = require('stream/promises')
+
+const App = () => {
+    const routes = new Map()
+    const createMyServer = () => createServer(serverHandler.bind(this))
+    const middlewaresForAll = []
+
+
+    async function* getAllStaticFiles(folder) {
+
+        const files = await readdir(folder)
+        for (const file of files) {
+            const absolutePath = path.join(folder, file)
+            if (statSync(absolutePath).isDirectory()) {
+                yield* getAllStaticFiles(absolutePath)
+            }
+            else {
+                yield absolutePath
+            }
+        }
+    }
+
+    const static = async (folderPath) => {
+        let folderRelative = folderPath.replace('./', '')
+        for await (const file of getAllStaticFiles(folderPath)) {
+            const pathWithoutBase = file.replace(folderRelative, '')
+            get(pathWithoutBase, async (req, res) => {
+                const relativePath = path.join(__dirname, '..', file)
+                const fileStream = createReadStream(relativePath)
+                res.setHeader('Content-Type', file.split('.').filter(Boolean).slice(1).join('.'))
+                return await pipeline(fileStream, res)
+            })
+        }
+    }
+
+    const use = (path, ...middlewares) => {
+        const possiblePaths = [path + '/GET', path + '/POST', path + '/PUT', path + '/PATCH', path + '/DELETE']
+        possiblePaths.forEach(route => {
+            const middlewaresAndControllers = routes.get(route) || []
+
+            if (middlewaresAndControllers.length) {
+                routes.set(route, [...middlewares, ...middlewaresAndControllers])
+            }
+        })
+    }
+
+    const useAll = (...middlewares) => {
+        middlewaresForAll.push(...middlewares)
+    }
+
+    const useRouter = (path, router) => {
+        const routerRoutes = router.getRoutes()
+        const middlewaresFromRouter = router.getMiddlewaresForAll()
+        const existentHandlers = routes.get(path) || []
+        routerRoutes.forEach((middlewares, key) => {
+            routes.set(`${path + key}`, [...existentHandlers, ...middlewaresFromRouter, ...middlewares])
+        })
+    }
+
+    const get = (path, ...handlers) => {
+        const currentHandlers = routes.get(`${path}/GET`) || [];
+        routes.set(`${path}/GET`, [...currentHandlers, ...handlers]);
+        if (path === "/about") {
+          console.log("path", path);
+          //return about section
+        }
+        if (path === "/recipe") {
+          console.log("path", path);
+          //return recipe content
+        }
+        if (path === "/blog") {
+          console.log("path", path);
+          //return blog content
+        }
+      };
+
+    const post = (path, ...handlers) => {
+        const currentHandlers = routes.get(`${path}/POST`) || []
+        routes.set(`${path}/POST`, [...currentHandlers, ...handlers])
+    }
+
+    const put = (path, ...handlers) => {
+        const currentHandlers = routes.get(`${path}/PUT`) || []
+        routes.set(`${path}/PUT`, [...currentHandlers, ...handlers])
+    }
+
+    const patch = (path, ...handlers) => {
+        const currentHandlers = routes.get(`${path}/PATCH`) || []
+        routes.set(`${path}/PATCH`, [...currentHandlers, ...handlers])
+    }
+
+    const del = (path, ...handlers) => {
+        const currentHandlers = routes.get(`${path}/DELETE`) || []
+        routes.set(`${path}/DELETE`, [...currentHandlers, ...handlers])
+    }
+
+    const dispatchChain = (request, response, middlewares) => {
+        return invokeMiddlewares(request, response, middlewares)
+    }
+
+    const invokeMiddlewares = async (request, response, middlewares) => {
+
+        if (!middlewares.length) return;
+
+        const currentMiddleware = middlewares[0];
+
+        return currentMiddleware(request, response, async () => {
+            await invokeMiddlewares(request, response, middlewares.slice(1));
+        })
+    }
+
+
+    const sanitizeUrl = (url, method) => {
+        const urlParams = url.split('/').slice(1)
+
+        // remove querystrings from the last parameter
+        const [lastParam] = urlParams[urlParams.length - 1].split('?')
+        urlParams.splice(urlParams.length - 1, 1)
+
+        // create the URL with our pattern
+        const allParams = [...urlParams, lastParam].join('/')
+        const sanitizedUrl = `/${allParams}/${method.toUpperCase()}`
+
+        return sanitizedUrl
+    }
+
+    const matchUrl = (sanitizedUrl) => {
+        for (const path of routes.keys()) {
+            const urlMatch = match(path, {
+                decode: decodeURIComponent,
+            })
+
+            const found = urlMatch(sanitizedUrl)
+
+            if (found) {
+                return path
+            }
+        }
+        return false
+    }
+
+
+    const serverHandler = async (request, response) => {
+        const sanitizedUrl = sanitizeUrl(request.url, request.method)
+
+        const match = matchUrl(sanitizedUrl)
+
+        if (match) {
+            const middlewaresAndControllers = routes.get(match)
+            await dispatchChain(request, response,
+                [requestDecorator.bind(null, routes.keys()), responseDecorator, ...middlewaresForAll, ...middlewaresAndControllers])
+        } else {
+            response.statusCode = 404
+            response.end('Not found')
+        }
+    }
+
+    const run = (port) => {
+        const server = createMyServer()
+        server.listen(port)
+    }
+
+
+    return {
+        run,
+        get,
+        post,
+        patch,
+        put,
+        del,
+        use,
+        useAll,
+        useRouter,
+        static
+    }
+}
+
+module.exports = App
